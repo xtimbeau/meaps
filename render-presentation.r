03fb4065fa260576bc722e96e04bd0a41c4783f6
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2023-03-19 23:37:00 +0100

    bugs and css

--- render-presentation.r
+++ render-presentation.r
@@ -2,9 +2,9 @@ library(purrr)
 library(stringr)
 
 output <- Sys.getenv("QUARTO_PROJECT_OUTPUT_DIR")
-pres <- c("MEAPS_p2203_fr")
+pres <- c("MEAPS_p1303_fr")
 walk(pres, ~{
-  quarto::quarto_render("MEAPS_p2203_fr.qmd")
+  quarto::quarto_render(str_c(.x, ".qmd"))
   file.copy(str_c(.x, ".html"), output, overwrite=TRUE)
   file.copy(str_c(.x, "_files"), output, overwrite=TRUE, recursive=TRUE)
   file.remove(str_c(.x, ".html"))
