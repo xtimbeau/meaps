3aa28625bc0425363862322e214370eec8ac072d
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2024-03-25 08:47:55 +0100

    update

--- _presentations/copyref.R
+++ _presentations/copyref.R
@@ -1,5 +1,6 @@
 bibs <- fs::dir_ls(glob="*.bib")
 fs::file_copy(bibs, "_presentations/", overwrite = TRUE)
+fs::file_copy("r/rinit.r", "_presentations/rinit.r", overwrite = TRUE)
 fs::dir_copy("secrets", "_presentations/secrets", overwrite = TRUE)
 fs::dir_copy("output", "_presentations/output", overwrite = TRUE)
 fs::dir_copy("_templates", "_presentations/_templates", overwrite = TRUE)
---
+++ _presentations/rinit.r
@@ -0,0 +1,46 @@
+library(knitr)
+opts_chunk$set(
+  echo = FALSE,
+  warning = FALSE,
+  message = FALSE,
+  fig.pos="htb", 
+  out.extra="",
+  dev="ragg_png",
+  out.width="100%",
+  fig.showtext=TRUE,
+  cache=FALSE)
+
+library(tidyverse)
+library(ofce)
+library(showtext)
+library(markdown)
+library(gt)
+library(qs)
+library(sf)
+library(ggspatial)
+library(stars)
+library(glue)
+library(gtsummary)
+library(scales)
+library(broom)
+library(broom.mixed)
+library(arrow)
+library(vroom)
+library(mapdeck)
+library(ggtext)
+
+options(
+  ofce.base_family = "Open Sans",
+  ofce.background_color = "transparent")
+
+tkn <- Sys.getenv("mapbox_token")
+mapdeck::set_token(tkn)
+
+style <- "mapbox://styles/xtimbeau/ckyx5exex000r15n0rljbh8od"
+showtext::showtext_opts(dpi = 180)
+showtext_auto()
+options(ofce.background_color = "grey99")
+options(ofce.base_family = "sans")
+options(ofce.base_size = 9)
+
+source("secrets/azure.R")
